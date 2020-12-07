6b4d8f7c59b694815024deaa97c55fb8f06454ad
Ian Culp <icculp@gmail.com>
2020-12-07 02:53:57 +0000

    Part 1 complete

---
+++ Day_6/part_1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+    Day 6 of Advent of Code 2020
+"""
+
+
+def part_one():
+    """ Counting answers """
+    with open('input') as input:
+        inp = input.readlines()
+
+    lines = []
+    temp = []
+    a = set([chr(i) for i in range(97, 123)])
+    for i in range(len(inp)):
+        if inp[i] == '\n':
+            lines.append(temp)
+            temp = []
+            continue
+        temp.append(inp[i])
+    '''print(lines)'''
+    lines.append(temp)
+    counts = []
+    '''lines = [['addddabcba\nbcdddddddddddddddddddddddddddddbcdedf', 'abcdef'], ['abcde', 'bcdeeeaa']]'''
+    print(len(lines))
+    for i in range(len(lines)):
+        temp = ''
+        for li in lines[i]:
+            temp += li
+        print(temp)
+        print(set(temp))
+        print(len(set(temp)))
+        b = a.intersection(set(temp))
+        counts.append(len(b))
+    print(counts)
+    return(sum(counts))
+
+
+if __name__ == '__main__':
+    sum = part_one()
+    print("\n\nSum: [{}]".format(sum))
