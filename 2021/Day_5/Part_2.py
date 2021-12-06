1b1795de4f35d23d8e42a3a3f114f3d0b08f66f6
Ian Culp <icculp@gmail.com>
2021-12-05 21:33:06 -0600

    Fixing part_2 code

--- 2021/Day_5/Part_2.py
+++ 2021/Day_5/Part_2.py
@@ -13,39 +13,48 @@ for line in numbers:
     #print(x1, x2, y1, y2)
     if x1 == x2:
         crossed[x1, np.minimum(y1, y2): np.maximum(y1, y2) + 1] += 1
+        print('hereeeee')
+        print(crossed[x1, np.minimum(y1, y2): np.maximum(y1, y2) + 1].shape)
+        # break
     elif y1 == y2:
         crossed[np.minimum(x1, x2): np.maximum(x1, x2) + 1, y1] += 1
+        print('herre')
+        print(crossed[np.minimum(x1, x2): np.maximum(x1, x2) + 1, y1].shape)
+        #break
     else:
-        ones = np.ones(crossed.shape)
-        diag = np.diag(ones)
-        np.diag(np.ones(4),1)
+        # ones = np.ones(crossed.shape)
+        # diag = np.diag(ones)
+        # np.diag(np.ones(4),1)
         if abs(x1 - x2) != abs(y1 - y2):
             print('wtf')
+        diag = np.eye(abs(x1 - x2) + 1)
         if x2 > x1 and y2 > y1:
             #print(11111111111, x1, y1, x2, y2)
-            diag = np.eye(x2 - x1 + 1)
             #print(diag)
-            crossed[x1:x2 + 1, x1:x2 + 1] += diag
+            crossed[np.minimum(x1, x2): np.maximum(x1, x2) + 1, np.minimum(y1, y2): np.maximum(y1, y2) + 1] += diag
         elif x1 > x2 or y1 > y2:
             #print(2, x1, y1, x2, y2)
             if x1 > x2:
-                diag = np.eye(x1 - x2 + 1)
                 if y1 > y2:
-                    crossed[x2 - 1:x1, y2:y1 + 1] += np.flip(diag, axis=1)
+                    print(diag.shape)
+                    print(line)
+                    print(y1, y2)
+                    print(crossed[np.minimum(x1, x2): np.maximum(x1, x2) + 1, np.minimum(y1, y2): np.maximum(y1, y2) + 1].shape)  
+                    crossed[np.minimum(x1, x2): np.maximum(x1, x2) + 1, np.minimum(y1, y2): np.maximum(y1, y2) + 1] += diag
                 else:
                     #print(x1, x2, y1, y2)
-                    #print(np.flip(diag, axis=1).shape)
-                    #print(crossed[x2:x1 + 1, y1:y2 + 1].shape)
-                    crossed[x2:x1 + 1, y1:y2 + 1] += np.flip(diag, axis=0)
+                    print(np.flip(diag, axis=1).shape)
+                    print(crossed[np.minimum(x1, x2): np.maximum(x1, x2) + 1, np.minimum(y1, y2): np.maximum(y1, y2) + 1].shape)
+                    crossed[np.minimum(x1, x2): np.maximum(x1, x2) + 1, np.minimum(y1, y2): np.maximum(y1, y2) + 1] += np.flip(diag, axis=1)
             elif y1 > y2:
-                diag = np.eye(y1 - y2 + 1)
+                # diag = np.eye(y1 - y2 + 1)
                 #print(np.flip(diag, axis=1))
-                crossed[x1:x2 + 1, y2:y1+1] += np.flip(diag, axis=0 )
+                crossed[np.minimum(x1, x2): np.maximum(x1, x2) + 1, np.minimum(y1, y2): np.maximum(y1, y2) + 1] += np.flip(diag, axis=1)
             #print(diag.shape)
             #print(crossed[x2:x1 + 1].shape)
         else:
             print('NOOOOOOOOOOO')
-print(crossed)
+print(crossed.T)
 print(np.array(np.where(crossed > 1)).shape)
 # 15140
 # 14501
@@ -53,15 +62,15 @@ print(np.array(np.where(crossed > 1)).shape)
 
 # 17193 actual...
 
-
+'''
 # borrowed from https://www.reddit.com/r/adventofcode/comments/r9824c/2021_day_5_solutions/hnd3a5v/?utm_source=reddit&utm_medium=web2x&context=3
 grid = np.zeros((2, 1000, 1000))
 #ls = np.fromregex(open(0), '\d+', [('',int)]*4)
 
 for (x, y, X, Y) in numbers:
-    dx, dy = np.sign([X-x, Y-y])                 
+    dxzzzzzzzzzzzzzzzzzzzzzzzz, dy = np.sign([X-x, Y-y])                 
     while (x,y) != (X+dx, Y+dy):
         grid[dx * dy, x, y] += 1
         x+=dx; y+=dy
 
-print((grid[0]>1).sum(), (grid.sum(0)>1).sum())
+print((grid[0]>1).sum(), (grid.sum(0)>1).sum())'''
